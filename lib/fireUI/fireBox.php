94266f9dc61182d4d76f4e1ed185f85417029248
Alisdair Rankine <info@alisdairrankine.com>
2011-03-08 18:53:53 +0000

    added new fireBox modal dialog class

---
+++ lib/fireUI/fireBox.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * This is a helper class to create modal dialogs
+ *
+ * @author alisdairrankine
+ */
+class fireBox {
+    //put your code here
+}
+?>
